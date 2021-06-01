71af03dbfdd787d44916c63490e5eedc944d010d
AMPILAHY Frelin (Canal Plus Prestataire) <frelin.ampilahy@cpexterne.org>
2021-06-01 17:21:18 +0200

    finalize

---
+++ src/main/java/com/slabcode/assessment/exception/GlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package com.slabcode.assessment.exception;
+
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @Bean
+    public ErrorAttributes errorAttributes() {
+        // Hide exception field in the return object
+        return new DefaultErrorAttributes() {
+            @Override
+            public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
+                Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, includeStackTrace);
+                if (errorAttributes.get("trace") != null) {
+                    String trace = errorAttributes.get("trace").toString();
+                    if (trace.length() > 255) {
+                        trace = trace.substring(0, 255);
+                    }
+                    if (trace.contains(";")) {
+                        trace = trace.substring(0, trace.lastIndexOf(";"));
+                    }
+                    errorAttributes.put("trace", trace);
+                }
+                errorAttributes.remove("exception");
+                // errorAttributes.remove("trace") //should uncomment this in production and document on returned http status
+                return errorAttributes;
+            }
+        };
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public void handleCustomException(HttpServletResponse res, CustomException ex) throws IOException {
+        res.sendError(ex.getHttpStatus().value(), ex.getMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public void handleAccessDeniedException(HttpServletResponse res) throws IOException {
+        res.sendError(HttpStatus.FORBIDDEN.value(), "Access denied");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public void handleException(HttpServletResponse res) throws IOException {
+        res.sendError(HttpStatus.BAD_REQUEST.value(), "Something went wrong");
+    }
+
+}
